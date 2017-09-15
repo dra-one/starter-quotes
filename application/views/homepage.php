2d6a0821e457f573618d08d3b3f2985b7d7912b5
dra-one <hsharma15@my.bcit.ca>
2017-09-14 21:58:46 -0700

    Synch with team repo
    Signed-off-by: dra-one <hsharma15@my.bcit.ca>

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,9 +1,9 @@
 <div class="row">
     {authors}
-    <div class="span4">
-        <a href="/welcome/show/{key}">
-            <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
-        </a>
-    </div>
+        <div class="span4">
+            <a href="/welcome/show/{key}">
+                <img src="/pix/{who}.jpg" title="{who} plays {who}"/>
+            </a>
+        </div>
     {/authors}
 </div>
